2a1ffb19f1c613c847355376b6320fa3b011bc43
Leandro Lara <leandrodasilvalara@yahoo.com.br>
2023-02-01 09:07:11 -0800

    layout boa vinda

--- Spotify/Managers/AuthManager.swift
+++ Spotify/Managers/AuthManager.swift
@@ -10,6 +10,11 @@ import Foundation
 final class AuthManager {
     static let shared = AuthManager()
     
+    struct Constants {
+        static let clientID = "3d24705d7738411b9e97bafcc69b73f9"
+        static let clientSecret = "48195c982acd4da7a8cb853eadb96a72"
+    }
+    
     private init() {}
     
     var isSignedIn: Bool {
