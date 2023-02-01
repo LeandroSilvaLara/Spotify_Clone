ea07d20384e79a583a3164d638d0b5f5c95e36bc
Leandro Lara <leandrodasilvalara@yahoo.com.br>
2023-01-31 22:10:02 -0800

    Initial Commit

---
+++ Spotify/Controllers/Core/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spotify
+//
+//  Created by Leandro Lara on 31/01/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Home"
+        view.backgroundColor = .red
+    }
+
+
+}
+
