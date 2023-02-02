938a94a73ecf0c1cb31271dd3cb721613718b4a1
Leandro Lara <leandrodasilvalara@yahoo.com.br>
2023-02-01 21:56:27 -0800

    atualizando parte de serviço.

--- Spotify/Managers/AuthManager.swift
+++ Spotify/Managers/AuthManager.swift
@@ -13,6 +13,7 @@ final class AuthManager {
     struct Constants {
         static let clientID = "3d24705d7738411b9e97bafcc69b73f9"
         static let clientSecret = "48195c982acd4da7a8cb853eadb96a72"
+        static let tokenAPIURL = "https://accounts.spotify.com/api/token"
     }
     
     private init () {}
@@ -44,6 +45,54 @@ final class AuthManager {
         completion: @escaping ((Bool) -> Void)
     ){
         // Get Token
+        guard let url = URL(string: Constants.tokenAPIURL) else {
+            return
+        }
+        
+        var componets = URLComponents()
+        componets.queryItems = [
+            URLQueryItem(name: "grant_type",
+                         value: "authorization_code"),
+            URLQueryItem(name: "code",
+                         value: code),
+            URLQueryItem(name: "redirect_uri",
+                         value: "https://www.iodevacademy.io"),
+        ]
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/x-www-from-urlencoded ",
+                         forHTTPHeaderField: "Content-Type")
+        request.httpBody = componets.query?.data(using: .utf8)
+        
+        let basicToken = Constants.clientID+":"+Constants.clientSecret
+        let data = basicToken.data(using: .utf8)
+        guard let base64String = data?.base64EncodedString() else {
+            print("Failure to get base64")
+            completion(false)
+            return
+        }
+        
+        request.setValue("Basic \(base64String)",
+                         forHTTPHeaderField: "Authorization")
+        
+        let task = URLSession.shared.dataTask(with: request) { data, _, error in
+            guard let data = data,
+                  error == nil else {
+                completion(false)
+                return
+            }
+            do {
+                let json = try JSONSerialization.jsonObject(with: data,
+                                                            options: .allowFragments)
+                print("SUCCESS: \(json)")
+            }
+            catch {
+                print (error.localizedDescription)
+                completion(false)
+            }
+        }
+        task.resume()
     }
     
     public func refreshAccessToken() {
