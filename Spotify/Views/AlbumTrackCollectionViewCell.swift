d81082d8ab05ea04496ee3c4afd1cbd078c9aae9
Leandro Lara <leandrodasilvalara@yahoo.com.br>
2023-02-06 20:44:27 -0800

    Adicionando parte layout para tela home

---
+++ Spotify/Views/AlbumTrackCollectionViewCell.swift
@@ -0,0 +1,112 @@
+//
+//  AlbumTrackCollectionViewCell.swift
+//  Spotify
+//
+//  Created by Leandro Lara on 06/02/23.
+//
+
+import Foundation
+import UIKit
+
+class AlbumTrackCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "AlbumTrackCollectionViewCell"
+    
+
+    private let trackLabel:UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 18, weight: .regular)
+        //label.backgroundColor = .red
+     
+        return label
+        
+    }()
+    private let trackNumberLabel:UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 18, weight: .semibold)
+     
+        return label
+        
+    }()
+
+    
+    private let artitstNameLabel:UILabel = {
+       
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 15, weight: .thin)
+       // label.backgroundColor = .green
+      
+        return label
+        
+    }()
+    
+    
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        contentView.backgroundColor = .secondarySystemBackground
+        contentView.addSubview(trackLabel)
+        contentView.addSubview(trackNumberLabel)
+        contentView.addSubview(artitstNameLabel)
+        
+        contentView.clipsToBounds = true
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+       // constraints
+       // trackLabel.sizeToFit()
+       // artitstNameLabel.sizeToFit()
+
+
+        
+        trackNumberLabel.frame = CGRect(
+            x:  20 ,
+            y: 15,
+            width: 30,
+            height: contentView.height / 2
+        )
+        
+        trackLabel.frame = CGRect(
+            x:  trackNumberLabel.right + 10 ,
+            y: 0,
+            width: contentView.width - trackNumberLabel.width,
+            height: (contentView.height / 1.5) - 10
+        )
+        artitstNameLabel.frame = CGRect(
+            x: trackNumberLabel.right + 10 ,
+            y: trackLabel.Bottom,
+            width: contentView.width - 15 ,
+            height: contentView.height - trackLabel.height
+        )
+    
+        
+        
+    }
+    
+    override func prepareForReuse(){
+        trackLabel.text = nil
+        artitstNameLabel.text = nil
+        trackNumberLabel.text = nil
+        
+        
+    }
+    
+    func Configure(with viewModel:AlbumCollectionViewCellViewModel , number: Int ){
+        trackLabel.text = viewModel.TrackName
+        artitstNameLabel.text = viewModel.artistName
+        trackNumberLabel.text =  String(number)
+    }
+    
+    
+}
+
---
+++ Spotify/Views/Browse/RecommendedTrackCollectionViewCell.swift
@@ -0,0 +1,105 @@
+//
+//  RecommendedTrackCollectionViewCell.swift
+//  Spotify
+//
+//  Created by Leandro Lara on 06/02/23.
+//
+
+import UIKit
+import SDWebImage
+
+class RecommendedTrackCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "RecommendedTrackCollectionViewCell"
+    
+    
+    private let imageOfTrack:UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "photo")
+//        imageView.layer.masksToBounds = true
+//        imageView.layer.cornerRadius = 8
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    private let trackLabel:UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 18, weight: .regular)
+     
+        return label
+        
+    }()
+    
+    private let artitstNameLabel:UILabel = {
+       
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 15, weight: .thin)
+      
+        return label
+        
+    }()
+    
+    
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        contentView.backgroundColor = .secondarySystemBackground
+        contentView.addSubview(imageOfTrack)
+        contentView.addSubview(trackLabel)
+        contentView.addSubview(artitstNameLabel)
+        contentView.clipsToBounds = true
+        contentView.layer.cornerRadius = 8
+        contentView.layer.masksToBounds = true
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+       // constraints
+        trackLabel.sizeToFit()
+        artitstNameLabel.sizeToFit()
+
+        let imageSize = contentView.height - 5
+        imageOfTrack.frame = CGRect(x: 0, y: 0, width: imageSize, height: imageSize)
+        
+        trackLabel.frame = CGRect(
+            x: imageOfTrack.right + 10 ,
+            y: 0,
+            width: contentView.width - imageOfTrack.right - 15,
+            height: contentView.height / 2
+        )
+        artitstNameLabel.frame = CGRect(
+            x: imageOfTrack.right + 10 ,
+            y: contentView.height / 2,
+            width: contentView.width - imageOfTrack.right-15 ,
+            height: contentView.height / 2
+        )
+    
+        
+        
+    }
+    
+    override func prepareForReuse(){
+        imageOfTrack.image = nil
+        trackLabel.text = nil
+        artitstNameLabel.text = nil
+        
+        
+    }
+    
+    func Configure(with viewModel:RecommentedTracksCellViewModel ){
+        imageOfTrack.sd_setImage(with: viewModel.trackImage, completed: nil)
+        trackLabel.text = viewModel.TrackName
+        artitstNameLabel.text = viewModel.artistName
+    }
+    
+    
+}
+
